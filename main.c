b843117d3b0387894d3ee4f92e43e6f896f65c22
Túlio Fernandes <tuliomf09@gmail.com>
2023-08-14 15:22:28 -0300

    feat: add initial usage with --help argument

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <gtk/gtk.h>
 
@@ -6,7 +7,21 @@ static void on_close_window() {
     gtk_main_quit();
 }
 
+static void usage() {
+    puts("Usage: img [OPÇÃO]... [ARQUIVO]...");
+    puts("A simple image viewer");
+    puts("\n  --help  display this help and exit\n");
+    puts("Examples:");
+    puts("\n  img /path/to/file");
+}
+
 int main(int argc, char **argv) {
+    if (argc > 1 && strcmp(argv[1], "--help") == 0) {
+        usage();
+
+        return 0;
+    }
+
     GtkWidget *window;
 
     gtk_init(&argc, &argv);
